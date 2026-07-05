11c73d19f740222c3eaae81539283bde6150b954
RepullesM <115624848+RepullesM@users.noreply.github.com>
2026-07-05 15:14:27 +0200

    Add files via upload

---
+++ fsc2-spp-pairs/1.array_bes.eth_SC.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#PBS -N FastimCoal2_besckei-ethilla_SC
+#PBS -l select=1:ncpus=6:mem=10gb:scratch_local=100gb
+#PBS -l walltime=96:00:00
+#PBS -e PBS/
+#PBS -o PBS/
+#PBS -J 0-99:20
+
+# Run ID
+start=$((PBS_ARRAY_INDEX + 1))
+end=$((PBS_ARRAY_INDEX + 20))
+# Scenario 
+PREFIX=SC
+DATADIR=/storage/brno12-cerit/home/repulles/03.Heliconius/Kay_pipeline/13.fsc2
+input=/storage/brno12-cerit/home/repulles/03.Heliconius/Kay_pipeline/07.SFS_VCF
+out=$DATADIR/bes.eth
+
+file=$input/Allsamples_nowallacei_Allsites_GENOME_jointMAFpop2_1.obs
+
+trap 'clean_scratch' TERM EXIT
+
+# Move to working directory
+cd $SCRATCHDIR
+
+#run 5 times (for more, make array)
+module load fastsimcoal2
+for i in $(seq $start $end)
+do
+  if [ $i -gt 100 ]; then
+    break
+  fi
+  mkdir ${PREFIX}_run$i
+  cd ${PREFIX}_run$i
+  cp ${out}/${PREFIX}.tpl ${out}/${PREFIX}.est .
+  cp ${file} ${PREFIX}_jointMAFpop1_0.obs
+  fsc27093 -t ${PREFIX}.tpl -n1000000 -m -e ${PREFIX}.est -M -L50 -q -c6
+  cd ..
+done
+
+cp -r ${PREFIX}_run* $out || export CLEAN_SCRATCH=false
